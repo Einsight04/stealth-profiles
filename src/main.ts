299733fba80f901310dc36d4b5b2b11b62c97cc7
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-08-19 07:49:58 -0400

    updates

--- src/main.ts
+++ src/main.ts
@@ -35,40 +35,39 @@ const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null
 });
-
-// add cookie
-const page = await browser.newPage();
-
-// delete all current cookies
-await page.deleteCookie(
-    ...await page.cookies()
-);
-
-interface Cookie {
-    name: string;
-    value: string;
-    domain: string;
-    path: string;
-    secure: boolean;
-    httpOnly: boolean;
-    sameSite: 'Strict' | 'Lax' | 'None';
-    expires: number;
-}
-
-const cookies: Cookie[] = JSON.parse(await fs.readFile(path.join(__dirname, '../resources', 'cookie.json'), 'utf8'));
-
-
-for (let cookie of cookies) {
-    await page.setCookie({
-        name: cookie.name,
-        value: cookie.value,
-        domain: cookie.domain,
-        path: cookie.path,
-        secure: cookie.secure,
-        httpOnly: cookie.httpOnly,
-        sameSite: cookie.sameSite,
-        expires: cookie.expires
-    });
-}
-
-await page.goto('https://www.chegg.com');
+//
+// // add cookie
+// const page = await browser.newPage();
+//
+// // delete all current cookies
+// await page.deleteCookie(
+//     ...await page.cookies()
+// );
+//
+// interface Cookie {
+//     name: string;
+//     value: string;
+//     domain: string;
+//     path: string;
+//     secure: boolean;
+//     httpOnly: boolean;
+//     sameSite: 'Strict' | 'Lax' | 'None';
+//     expires: number;
+// }
+//
+// const cookies: Cookie[] = JSON.parse(await fs.readFile(path.join(__dirname, '../resources', 'cookie.json'), 'utf8'));
+//
+// for (let cookie of cookies) {
+//     await page.setCookie({
+//         name: cookie.name,
+//         value: cookie.value,
+//         domain: cookie.domain,
+//         path: cookie.path,
+//         secure: cookie.secure,
+//         httpOnly: cookie.httpOnly,
+//         sameSite: cookie.sameSite,
+//         expires: cookie.expires
+//     });
+// }
+//
+// await page.goto('https://www.chegg.com');

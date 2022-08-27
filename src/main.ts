b38d2a4b8bdb2d1598e3559147ab26e8b32077b4
EinsightOG <93455330+EinsightOG@users.noreply.github.com>
2022-08-27 12:32:40 -0400

    updates

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,57 @@ puppeteer.use(StealthPlugin());
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
 
-// const browserProfiles: number[] = [
-//     1
-// ]
-//
-// const files = await fs.readdir(path.join(__dirname, '../resources', 'stealth-browsers'));
-//
-// for (let file of files) {
-//     await fs.rmdir(path.join(__dirname, '../resources', 'stealth-browsers', file), {recursive: true});
-// }
-//
-// for (let profile of browserProfiles) {
-//     await puppeteer.launch({
-//         executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
-//         userDataDir: path.join(__dirname, '../resources', `stealth-browsers\\context-${profile}`),
-//         headless: false,
-//         defaultViewport: null
-//     });
-// }
+interface Cookie {
+    name: string;
+    value: string;
+    domain: string;
+    path: string;
+    secure: boolean;
+    httpOnly: boolean;
+    sameSite: 'Strict' | 'Lax' | 'None';
+    expires: number;
+}
+
+// browser profiles to generate
+const browserProfiles: number[] = [
+    2, 3, 4, 5
+]
+
+const cookies: string[] = await fs.readdir(path.join(__dirname, '..', 'resources/cookies'));
+
+for (let i = 0; i < browserProfiles.length; i++) {
+    const cookie: any[] = JSON.parse(await fs.readFile(path.join(__dirname, '../resources', cookies[i]), 'utf8'));
+
+    // replace all sameSite values to corresponding value
+    for (let j = 0; j < cookie.length; j++) {
+        switch (cookie[j].sameSite) {
+            case 'lax':
+                cookie[j].sameSite = 'Lax';
+                break;
+            case 'no_restriction':
+                cookie[j].sameSite = 'None';
+                break;
+            case null:
+                cookie[j].sameSite = 'None';
+                break;
+        }
+    }
+
+    await puppeteer.launch({
+        executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+        userDataDir: path.join(__dirname, '../resources', `stealth-browsers\\context-${browserProfiles[i]}`),
+        headless: false,
+        defaultViewport: null
+    });
+}
 
 
-const browser = await puppeteer.launch({
-    executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
-    userDataDir: 'C:\\Users\\ghait\\IdeaProjects\\cheggy-backend\\src\\main\\resources\\stealth-browsers\\context-1',
-    headless: false,
-    defaultViewport: null
-});
+// const browser = await puppeteer.launch({
+//     executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+//     userDataDir: 'C:\\Users\\ghait\\IdeaProjects\\cheggy-backend\\src\\main\\resources\\stealth-browsers\\context-1',
+//     headless: false,
+//     defaultViewport: null
+// });
 //
 // // add cookie
 // const page = await browser.newPage();
